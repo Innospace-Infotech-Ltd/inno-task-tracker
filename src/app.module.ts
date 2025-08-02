af3c4868ce4677e5d47e67736ebb492cdae3aef8
Rojon Ali <rojon@particularaudience.com>
2025-08-02 13:24:56 +0600

    Integrate ThrottlerModule for rate limiting and improve module imports in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,25 @@ import { AuthModule } from './auth/auth.module';
 import { TasksModule } from './tasks/tasks.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import * as dotenv from 'dotenv';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 dotenv.config();
 
 @Module({
   imports: [
-    MongooseModule.forRoot(process.env.MONGO_URI || 'mongodb://mongo:27017/inno-task-tracker'),
-    HealthModule, AuthModule, TasksModule],
+    MongooseModule.forRoot(
+      process.env.MONGO_URI || 'mongodb://mongo:27017/inno-task-tracker',
+    ),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+    HealthModule,
+    AuthModule,
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
